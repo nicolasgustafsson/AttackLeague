fa19c9d3a0bdf61d387cefb46d624110f0f4398f
Nico <nicolas.gustafsson@R9P646G.learnet.se>
2017-08-22 21:03:22 +0200

    extra commi for cool people

--- AttackLeague/AttackLeague/AttackLeague/Grid.cs
+++ AttackLeague/AttackLeague/AttackLeague/Grid.cs
@@ -9,10 +9,29 @@ namespace AttackLeague.AttackLeague
     public class Grid
     {
         private List<List<Tile>> myGrid;
+        private List<AbstractBlock> myBlocks;
         private int myHeight = 12;
         private int myWidth = 6;
 
+        public Grid()
+        {
+            for ( int rows = 0; rows < myHeight; ++rows)
+            {
+                myGrid.Add(new List<Tile>());
+                for (int columns = 0; columns < myWidth; ++columns)
+                {
+                    myGrid[rows].Add(new Tile());
+                }
+            }
+        }
 
+        public void Update()
+        {
+            foreach (AbstractBlock iBlock in myBlocks)
+            {
+
+            }
+        }
 
 
     }
